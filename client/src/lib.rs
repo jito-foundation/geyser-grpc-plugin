5ca7869aa4ca285cb1cbc6dbc5eee11cccaffa6b
segfaultdoctor <17258903+segfaultdoc@users.noreply.github.com>
2022-11-23 14:34:54 -0500

    update client code structure (#2)

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -7,34 +7,30 @@ use std::{
 };
 
 use solana_sdk::{clock::Slot, pubkey::Pubkey};
-use tokio::runtime::Runtime;
 use tonic::transport::{ClientTlsConfig, Endpoint};
 
 use crate::{geyser_consumer::GeyserConsumer, geyser_proto::geyser_client::GeyserClient};
 
-pub(crate) mod geyser_proto {
+pub mod geyser_proto {
     tonic::include_proto!("geyser");
 }
 
-pub fn connect(
-    runtime: Arc<Runtime>,
+pub async fn connect(
     geyser_addr: String,
     tls_config: Option<ClientTlsConfig>,
     exit: Arc<AtomicBool>,
 ) -> GeyserConsumer {
     let endpoint = Endpoint::from_str(&geyser_addr).unwrap();
-    let ch = runtime.block_on(async {
-        if let Some(tls) = tls_config {
-            endpoint.tls_config(tls).expect("tls_config")
-        } else {
-            endpoint
-        }
-        .connect()
-        .await
-        .expect("failed to connect")
-    });
+    let ch = if let Some(tls) = tls_config {
+        endpoint.tls_config(tls).expect("tls_config")
+    } else {
+        endpoint
+    }
+    .connect()
+    .await
+    .expect("failed to connect");
 
     let c = GeyserClient::new(ch);
 
-    GeyserConsumer::new(c, runtime, exit)
+    GeyserConsumer::new(c, exit)
 }
