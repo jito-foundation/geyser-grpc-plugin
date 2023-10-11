b2bbab00eedc3682e1425ad67d4be18d0cdb1ab7
Lucas B <lucas@jito.wtf>
2023-10-11 13:15:22 -0500

    build

--- proto/build.rs
+++ proto/build.rs
@@ -2,6 +2,14 @@ use tonic_build::configure;
 
 fn main() {
     configure()
+        .type_attribute(
+            "TransactionErrorType",
+            "#[cfg_attr(test, derive(enum_iterator::Sequence))]",
+        )
+        .type_attribute(
+            "InstructionErrorType",
+            "#[cfg_attr(test, derive(enum_iterator::Sequence))]",
+        )
         .compile(
             &[
                 "proto/geyser.proto",
